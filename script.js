a3d64d70fe1ec94c077d165436cf41eb32f3620c
GiulsV <valente.giulia93@gmail.com>
2022-05-18 15:37:56 +0200

    Script esercizio

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-// genera un array di 10 numeri casuali range 1- 100, ma stavolta senza doppioni
 
-// Crea un array vuoto e chiedi all’utente un numero da inserire nell’array. Continua a chiedere i numeri all’utente e a inserirli nell’array fino a quando la somma degli elementi è minore di 50.
+// Crea un array vuoto e chiedi all’utente un numero da inserire nell’array. 
+// Continua a chiedere i numeri all’utente e a inserirli nell’array fino a quando la somma degli elementi è minore di 50.
+
+let arrayNumeriUtente=[];
+
+let sommaNumeriUtente = 0;
+
+while(sommaNumeriUtente < 50){
+
+    let numeroUtente = parseInt(prompt("Inserisci un numero"));
+
+    arrayNumeriUtente.push(numeroUtente);
+
+    sommaNumeriUtente += numeroUtente;
+
+    console.log(arrayNumeriUtente);
+
+}
+
+console.log(sommaNumeriUtente);
